8d0279a5572b1367c0aa0dd6edb64d5276863936
“nicm42” <42087659+nicm42@users.noreply.github.com>
2023-12-29 20:48:12 +0000

    PIcking up Google Sheets data in React

--- src/server.js
+++ src/server.js
@@ -5,14 +5,16 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 const key = process.env.API_KEY;
-const link = `https://sheets.googleapis.com/v4/spreadsheets/1jXvN4YvrQvCbY3ektHfJKRUEk5-aLmFkDz7SXzlLZEw/values/2023?valueRenderOption=FORMATTED_VALUE&key=${key}`;
+const sheetID = '1jXvN4YvrQvCbY3ektHfJKRUEk5-aLmFkDz7SXzlLZEw';
+//const link = `https://sheets.googleapis.com/v4/spreadsheets/1jXvN4YvrQvCbY3ektHfJKRUEk5-aLmFkDz7SXzlLZEw/values/2023?valueRenderOption=FORMATTED_VALUE&key=${key}`;
+const link = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}?key=${key}`;
 
 // Serve API request
-app.get('/', async (req, res) => {
+app.get('/api', async (req, res) => {
   const response = await fetch(link);
   const data = await response.json();
-  //console.log(data.values);
-  //res.send(data.values);
+  console.log(data.sheets);
+  res.send(data.sheets);
 });
 
 app.listen(port, () => {
