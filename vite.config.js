f01975ccf324762b2de9af79486d06a77c5ed775
“nicm42” <42087659+nicm42@users.noreply.github.com>
2023-12-30 19:38:47 +0000

    Installed and set up ESLint, Prettier, Stylelint, Vitest and Cypress

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,10 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/test-setup.js',
+    },
   },
 });
